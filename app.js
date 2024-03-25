33f9047f39176dc86f6f57d6de6ffb7543fd1f6c
MJDaws0n <maxjdawson7@gmail.com>
2024-03-25 16:36:27 +0000

    Added code and updated gitignore

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
-// Import necessary modules
-// const fs = require('fs');
-// const { NeuralNetwork } = require('brain.js');
+const brain = require('./brain.js');
 
-// // Check if the model file exists
-// let net;
-// const modelFile = 'model.json';
-// if (fs.existsSync(modelFile)) {
-//     // Load the model from model.json if it exists
-//     const modelJson = fs.readFileSync(modelFile, 'utf8');
-//     net = new NeuralNetwork().fromJSON(JSON.parse(modelJson));
-// } else {
-//     // Create a new neural network if the model file doesn't exist
-//     const config = {
-//         hiddenLayers: [10]
-//     };
-//     net = new NeuralNetwork(config);
-// }
-
-// // Define training data for XOR operation
-// const trainingData = [
-//   { input: [0], output: [0] },
-
-// ];
-
-// // Train the neural network with the training data
-// net.train(trainingData, {
-//     log: (details) => console.log(details),
-//     iterations: 1000000
-// });
-
-// // Save the trained model to model.json
-// const modelJson = net.toJSON();
-// fs.writeFileSync(modelFile, JSON.stringify(modelJson), 'utf8');
-
-console.log(convert('#   #   '));
-
-function convert(input){
-    input = input.replace(/#/, '1').replace(/ /, '0');
-    return input.split('');
+// Function to convert '#' to 1 and ' ' to 0
+function preprocessInput(input) {
+  return input.replace(/#/g, '1').replace(/ /g, '0').split('').map(str => parseInt(str, 10));
 }
+
+// Define training data
+const trainingData = [
+  { input: preprocessInput(
+    "########" +
+    "#      #" +
+    "#      #" +
+    "#      #" +
+    "#      #" +
+    "#      #" +
+    "########"
+  ), output: { zero: 1 } },
+  { input: preprocessInput(
+    "   #    " +
+    "   #    " +
+    "   #    " +
+    "   #    " +
+    "   #    " +
+    "   #    " +
+    "   #    "
+  ), output: { one: 1 } }
+];
+
+// Create a neural network
+const net = new brain.NeuralNetwork();
+
+// Train the neural network
+net.train(trainingData);
+
+// Test the neural network
+const testData = preprocessInput(
+    "   #    " +
+    "   #    " +
+    "   #    " +
+    "   #    " +
+    "   #    " +
+    "   #    " +
+    "   #    "
+);
+
+const output = net.run(testData);
+
+console.log(output);
